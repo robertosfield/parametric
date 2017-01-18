a2b8f3d2ec863bfcc7556d0038883850b47eaea2
Robert Osfield <robert@openscenegraph.com>
2017-01-18 15:01:51 +0000

    Removed old functions that are no longer used

--- main.cpp
+++ main.cpp
@@ -18,11 +18,6 @@
 #include "ParametricScene.h"
 
 
-using namespace osgParametric;
-
-typedef std::vector< osg::ref_ptr<osg::Texture2D> > Textures;
-
-
 osg::ref_ptr<osg::Geometry> createMesh(const osg::Vec3& origin, const osg::Vec3& uAxis, const osg::Vec3& vAxis, unsigned int uCells, unsigned vCells, bool top)
 {
     osg::ref_ptr<osg::Geometry> geometry = new osg::Geometry;
@@ -227,57 +222,6 @@ osg::ref_ptr<osg::Geometry> createSideWalls(const osg::Vec3& baseOrigin, const o
     return geometry;
 }
 
-osg::ref_ptr<osg::Texture2D> createDepthTexture(unsigned int width, unsigned int height)
-{
-    osg::ref_ptr<osg::Texture2D> depthTexture = new osg::Texture2D;
-    depthTexture->setTextureSize(width, height);
-    depthTexture->setInternalFormat(GL_DEPTH_COMPONENT);
-    depthTexture->setFilter(osg::Texture2D::MIN_FILTER,osg::Texture2D::LINEAR);
-    depthTexture->setFilter(osg::Texture2D::MAG_FILTER,osg::Texture2D::LINEAR);
-    depthTexture->setWrap(osg::Texture2D::WRAP_S,osg::Texture2D::CLAMP_TO_BORDER);
-    depthTexture->setWrap(osg::Texture2D::WRAP_T,osg::Texture2D::CLAMP_TO_BORDER);
-    depthTexture->setBorderColor(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
-    return depthTexture;
-}
-
-
-osg::ref_ptr<osg::Camera> createDepthCamera(parameter_ptr<osg::Texture> depthTexture, bool backFace)
-{
-    osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-    camera->attach(osg::Camera::DEPTH_BUFFER, depthTexture.get());
-    camera->setViewport(0, 0, depthTexture->getTextureWidth(), depthTexture->getTextureHeight());
-
-    // clear the depth and colour bufferson each clear.
-    camera->setClearMask(GL_DEPTH_BUFFER_BIT);
-
-    // set the camera to render before the main camera.
-    camera->setRenderOrder(osg::Camera::PRE_RENDER);
-
-    // tell the camera to use OpenGL frame buffer object where supported.
-    camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
-
-    camera->setReferenceFrame(osg::Transform::RELATIVE_RF);
-    camera->setProjectionMatrix(osg::Matrixd::identity());
-    camera->setViewMatrix(osg::Matrixd::identity());
-
-    camera->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR);
-
-    camera->setCullCallback(new osgParametric::RTTCameraCullCallback());
-
-    if (backFace)
-    {
-        camera->getOrCreateStateSet()->setAttribute(new osg::Depth(osg::Depth::GREATER));
-        camera->setClearDepth(0.0);
-    }
-    else
-    {
-//        camera->getOrCreateStateSet()->setAttribute(new osg::Depth(osg::Depth::LESS));
-//        camera->setClearDepth(1.0);
-    }
-
-    return camera;
-}
-
 osg::ref_ptr<osg::Program> createProgram(osg::ArgumentParser& arguments)
 {
     osg::ref_ptr<osg::Program> program = new osg::Program;
@@ -303,35 +247,6 @@ osg::ref_ptr<osg::Program> createProgram(osg::ArgumentParser& arguments)
     }
 
     return program;
-}
-
-void setUpDepthStateSet(osg::StateSet* stateset, Textures& backFaceDepthTextures, Textures& frontFaceDepthTextures, unsigned int width, unsigned int height)
-{
-    std::stringstream name;
-    int unit=0;
-    unsigned int numDepthTextures = std::min(backFaceDepthTextures.size(), frontFaceDepthTextures.size());
-    for(unsigned int i=0; i<numDepthTextures; ++i)
-    {
-        name<<"frontDepthTexture"<<i;
-        stateset->setTextureAttribute(unit, frontFaceDepthTextures[i].get());
-        stateset->addUniform(new osg::Uniform(name.str().c_str(), unit));
-
-        name.str("");
-        ++unit;
-
-        name<<"backDepthTexture"<<i;
-        stateset->setTextureAttribute(unit, backFaceDepthTextures[i].get());
-        stateset->addUniform(new osg::Uniform(name.str().c_str(), unit));
-
-        name.str("");
-        ++unit;
-    }
-
-    name.str("");
-    name<<numDepthTextures;
-    stateset->setDefine("NUM_DEPTH_TEXTURES", name.str());
-
-    stateset->addUniform(new osg::Uniform("viewportDimensions",osg::Vec4(0.0f,0.0f,static_cast<float>(width),static_cast<float>(height))));
 }
 
 osg::ref_ptr<osg::Group> createParametric(osg::ArgumentParser& arguments)
