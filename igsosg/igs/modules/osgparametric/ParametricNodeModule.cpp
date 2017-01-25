fc8aafcf0a4ba25d6dc8678281dde5f58e660059
csklu <csklu@cesiumsolutions.com>
2017-01-25 22:30:12 +0200

    OpenIGS module interface added.

---
+++ igsosg/igs/modules/osgparametric/ParametricNodeModule.cpp
@@ -0,0 +1,170 @@
+#include "ParametricNodeModule.hpp"
+#include <igs/modules/osg/scenegraph/GroupModule.hpp>
+
+#include <igs/modules/osg/OSGDefs.hpp>
+#include <igs/modules/osg/SmartPtrOSGDefs.hpp>
+#include <igs/modules/osg/SmartPtrInfoOSGDefs.hpp>
+#include <igs/database/ModuleDevSource.hpp>
+
+// ----------------------------------------------------------------------------
+// ParametricNode Module definitions
+// ----------------------------------------------------------------------------
+
+namespace igs {
+
+template <>
+ClassInfo const &
+classInfo<osgParametric::ParametricNode>()
+{
+  static ClassInfo info = ClassInfo().init<osgParametric::ParametricNode>()
+
+      .addConstructor( constructor< osgParametric::ParametricNode, osg::ref_ptr<osgParametric::ParametricNode> >() )
+
+      .addProperty(
+          Property( "Dimensions", Property::Persistent,
+              Method("getDimensions", &osgParametric::ParametricNode::getDimensions,
+                  Parameter("width", 1000, Parameter::Out),
+                  Parameter("height", 1000, Parameter::Out)
+              ),
+              Method( "setDimensions", &osgParametric::ParametricNode::setDimensions,
+                  Parameter("width", 1000, Parameter::In),
+                  Parameter("height", 1000, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "NumCells", Property::Persistent,
+              Method("getNumCells", &osgParametric::ParametricNode::getNumCells,
+                  Parameter("uCells", 10, Parameter::Out),
+                  Parameter("vCells", 10, Parameter::Out)
+              ),
+              Method( "setNumCells", &osgParametric::ParametricNode::setNumCells,
+                  Parameter("uCells", 10, Parameter::In),
+                  Parameter("vCells", 10, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "RenderBase", Property::Persistent,
+              Member("getRenderBase", &osgParametric::ParametricNode::renderBase,
+                  Return("renderBase", false, Return::Out)
+              ),
+              Member("setRenderBase", &osgParametric::ParametricNode::renderBase,
+                  Parameter("renderBase", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "RenderTop", Property::Persistent,
+              Member("getRenderTop", &osgParametric::ParametricNode::renderTop,
+                  Return("renderTop", true, Return::Out)
+              ),
+              Member("setRenderTop", &osgParametric::ParametricNode::renderTop,
+                  Parameter("renderTop", true, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "RenderSidewalls", Property::Persistent,
+              Member("getRenderSidewalls", &osgParametric::ParametricNode::renderSidewalls,
+                  Return("renderSidewalls", false, Return::Out)
+              ),
+              Member("setRenderSidewalls", &osgParametric::ParametricNode::renderSidewalls,
+                  Parameter("renderSidewalls", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "VisibleBoundaries", Property::Persistent,
+              Member("getVisibleBoundaries", &osgParametric::ParametricNode::visibleBoundaries,
+                  Return("visibleBoundaries", false, Return::Out)
+              ),
+              Member("setVisibleBoundaries", &osgParametric::ParametricNode::visibleBoundaries,
+                  Parameter("visibleBoundaries", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "DepthBoundaries", Property::Persistent,
+              Member("getDepthBoundaries", &osgParametric::ParametricNode::depthBoundaries,
+                  Return("depthBoundaries", false, Return::Out)
+              ),
+              Member("setDepthBoundaries", &osgParametric::ParametricNode::depthBoundaries,
+                  Parameter("depthBoundaries", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "ZFunction", Property::Persistent,
+              Member("getZFunction", &osgParametric::ParametricNode::zFunction,
+                  Return("zFunction", false, Return::Out)
+              ),
+              Member("setZFunction", &osgParametric::ParametricNode::zFunction,
+                  Parameter("zFunction", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "ZBase", Property::Persistent,
+              Member("getZBase", &osgParametric::ParametricNode::zBase,
+                  Return("zBase", false, Return::Out)
+              ),
+              Member("setZBase", &osgParametric::ParametricNode::zBase,
+                  Parameter("zBase", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "ZTop", Property::Persistent,
+              Member("getZTope", &osgParametric::ParametricNode::zTop,
+                  Return("zTop", false, Return::Out)
+              ),
+              Member("setZTop", &osgParametric::ParametricNode::zTop,
+                  Parameter("zTop", false, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "Subgraph", Property::Persistent,
+              Operation(),
+              Method("addSubgraph", &osgParametric::ParametricNode::addSubgraph,
+                  Parameter("subgraph", (osg::Node *)0, Parameter::In)
+              ),
+              Signal()
+          )
+      )
+
+      .addProperty(
+          Property( "Setup", Property::Persistent,
+              Operation(),
+              Method("setup", &osgParametric::ParametricNode::setup),
+              Signal()
+          )
+      )
+  ;
+  return info;
+} // classInfo<osgParametric::ParametricNode>
+
+} // namespace igs
+
+DEFINE_MODULE_CLASS( osgParametric::ParametricNode, sg, ParametricNode );
